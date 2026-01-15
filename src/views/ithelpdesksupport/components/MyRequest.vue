4bbe66c5e5c1d82b9b76fecaa600cc96499703d1
nur indah <nurindahch022@gmail.com>
2026-01-15 13:00:06 +0700

    Penambahan fitur dan perbaikan tampilan dalam mdoul IT Helpdesk bagian My Request

--- src/views/ithelpdesksupport/components/MyRequest.vue
+++ src/views/ithelpdesksupport/components/MyRequest.vue
@@ -1,134 +1,158 @@
 <template>
-  <div>
-    <v-row style="background-color: #f1f1f7">
-      <v-col @click="filterStatus(null)" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info all-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiTicketConfirmationOutline }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">All Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ allTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col @click="filterStatus('ASSIGNED')" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info assigned-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiClipboardArrowRightOutline }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">Assigned Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ assignedTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col @click="filterStatus('INPROGRESS')" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info progress-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiLoading }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">Progress Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ progressTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col @click="filterStatus('PENDING')" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info pending-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiClipboardTextClockOutline }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">Pending Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ pendingTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col @click="filterStatus('LATE')" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info late-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiClipboardAlertOutline }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">Late Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ lateTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col @click="filterStatus('RESOLVED')" cols="12" sm="6" md="4" lg="2">
-        <v-card class="detail-info solved-ticket">
-          <v-card-text>
-            <v-icon style="color: white">{{ icons.mdiCheckDecagramOutline }}</v-icon><br />
-            <span style="display: -webkit-inline-box; color: white">Solved Tickets</span>
-            <br />
-            <p style="font-weight: bold; font-size: x-large; color: white">{{ solvedTickets }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+  <div class="helpdesk-container">
+    <!-- Status Cards Section -->
+    <div class="status-cards-section">
+      <v-row class="status-cards-row ma-0">
+        <v-col 
+          v-for="(stat, index) in statusCards" 
+          :key="index"
+          @click="filterStatus(stat.filter)" 
+          cols="6" 
+          sm="6" 
+          md="4" 
+          lg="2" 
+          class="status-col"
+        >
+          <v-card :class="['detail-info', stat.class]">
+            <v-card-text class="pa-3 pa-sm-4">
+              <v-icon color="white" :size="iconSize">{{ icons[stat.icon] }}</v-icon>
+              <div class="status-title mt-2">{{ stat.title }}</div>
+              <div class="status-value mt-1">{{ stat.value }}</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
 
-    <v-card class="mt-4">
-      <v-row class="px-4">
-        <v-spacer />
-        <v-col cols="12" sm="6" md="2" lg="2">
-          <v-select dense v-model="catalogId" outlined :items="dataCatalog" label="Catalog" clearable></v-select>
-        </v-col>
-        <v-col cols="12" sm="6" md="2" lg="2">
-          <date-picker :format="dateFormat" v-model="startDate" format="DD-MM-YYYY" placeholder="Start Date"
-            class="datetime-picker"></date-picker>
-        </v-col>
-        <v-col cols="12" sm="6" md="2" lg="2">
-          <date-picker :format="dateFormat" v-model="endDate" :disabled-date="disabledFromStartDate" format="DD-MM-YYYY"
-            placeholder="End Date" class="datetime-picker"></date-picker>
-        </v-col>
-        <v-col v-if="!isITLead" cols="12" sm="6" md="6" lg="6">
-          <v-text-field dense :append-icon="icons.mdiMagnify" v-model="keyword" outlined
-            label="Search by ticket number or title" clearable></v-text-field>
-        </v-col>
-        <v-col v-if="isITLead" cols="12" sm="6" md="4" lg="4">
-          <v-text-field dense :append-icon="icons.mdiMagnify" v-model="keyword" outlined
-            label="Search by ticket number or title" clearable></v-text-field>
-        </v-col>
-        <v-col v-if="isITLead" cols="12" sm="6" md="2" lg="2">
-          <v-btn class="btn-blue" :loading="exportLoading" @click="downloadExcel" depressed>
-            Download Excel
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row class="px-4">
-        <v-spacer></v-spacer>
-        <v-col v-if="isITLead" cols="2">
-          <v-btn class="btn-blue" @click="addTicket" depressed>
-            New Request
-          </v-btn>
-        </v-col>
-      </v-row>
+    <!-- Filters and Table Section -->
+    <v-card class="main-card mt-3">
+      <!-- Filter Controls -->
+      <v-card-text class="pa-3 pa-sm-4">
+        <v-row class="ma-0">
+          <v-col cols="12" sm="6" md="3" class="pa-1 pa-sm-2">
+            <v-select 
+              dense 
+              v-model="catalogId" 
+              outlined 
+              :items="dataCatalog" 
+              label="Catalog" 
+              clearable
+              hide-details
+            ></v-select>
+          </v-col>
+          <v-col cols="6" sm="6" md="3" class="pa-1 pa-sm-2">
+            <date-picker 
+              :format="dateFormat" 
+              v-model="startDate" 
+              format="DD-MM-YYYY" 
+              placeholder="Start Date"
+              class="datetime-picker"
+            ></date-picker>
+          </v-col>
+          <v-col cols="6" sm="6" md="3" class="pa-1 pa-sm-2">
+            <date-picker 
+              :format="dateFormat" 
+              v-model="endDate" 
+              :disabled-date="disabledFromStartDate" 
+              format="DD-MM-YYYY"
+              placeholder="End Date" 
+              class="datetime-picker"
+            ></date-picker>
+          </v-col>
+          <v-col v-if="!isITLead" cols="12" md="3" class="pa-1 pa-sm-2">
+            <v-text-field 
+              dense 
+              :append-icon="icons.mdiMagnify" 
+              v-model="keyword" 
+              outlined
+              label="Search ticket" 
+              clearable
+              hide-details
+            ></v-text-field>
+          </v-col>
+          <v-col v-if="isITLead" cols="6" sm="6" md="3" class="pa-1 pa-sm-2">
+            <v-btn 
+              class="btn-blue" 
+              :loading="exportLoading" 
+              @click="downloadExcel" 
+              depressed
+              block
+              small
+            >
+              <v-icon left small>{{ icons.mdiDownload }}</v-icon>
+              <span class="hidden-xs-only">Download Excel</span>
+              <span class="hidden-sm-and-up">Excel</span>
+            </v-btn>
+          </v-col>
+        </v-row>
 
-      <v-row style="padding: 1%">
-        <v-data-table hide-default-footer :loading="loading" style="width: 100%" :headers="headers" :items="tickets"
-          @click:row="handleClick" :footer-props="{
-            'items-per-page-options': [5, 10, 25],
-            'items-per-page-text': 'Items per page:',
-            'show-current-page': true,
-            'show-first-last-page': true,
-          }">
-          <template #[`item.createdAt`]="{ item }">
-            <span>{{ formatDate(item.createdAt) }}</span>
-          </template>
-          <template #[`item.rating`]="{ item }">
-            {{ item.rating !== "" && item.rating !== null ? "Yes" : "No" }}
-          </template>
-          <template #[`item.statusName`]="{ item }">
-            <span class="pa-2 status-badge" :class="getStatusClass(item.statusId)">
-              {{ item.statusName }}
-              <v-icon color="white">{{ getStatusIcon(item.statusId) }}</v-icon>
-            </span>
-          </template>
-        </v-data-table>
-      </v-row>
-      <div class="ml-4 mb-4" style="display: flex; justify-content: space-between">
-        <span style="font-weight: 700; align-self: center">Total Data : {{ totalItems }}</span>
-        <v-pagination v-model="pages" :length="totalPage" @input="onPageChangeDetil" :total-visible="7"></v-pagination>
-      </div>
+        <!-- New Request Button -->
+        <v-row v-if="isITLead" class="ma-0 mt-2">
+          <v-spacer></v-spacer>
+          <v-col cols="12" sm="6" md="3" class="pa-1 pa-sm-2">
+            <v-btn class="btn-blue" @click="addTicket" depressed block small>
+              <v-icon left small>{{ icons.mdiPlus }}</v-icon>
+              New Request
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <!-- Data Table -->
+      <v-card-text class="pa-0 pa-sm-2">
+        <div class="table-container">
+          <v-data-table 
+            hide-default-footer 
+            :loading="loading" 
+            :headers="tableHeaders" 
+            :items="tickets"
+            @click:row="handleClick" 
+            :mobile-breakpoint="0"
+            class="elevation-0"
+          >
+            <template #[`item.createdAt`]="{ item }">
+              <span class="text-no-wrap">{{ formatDate(item.createdAt) }}</span>
+            </template>
+            <template #[`item.rating`]="{ item }">
+              {{ item.rating !== "" && item.rating !== null ? "Yes" : "No" }}
+            </template>
+            <template #[`item.statusName`]="{ item }">
+              <v-chip 
+                small 
+                :class="getStatusClass(item.statusId)"
+                class="status-chip"
+              >
+                <span class="status-text">{{ item.statusName }}</span>
+                <v-icon color="white" x-small class="ml-1">
+                  {{ getStatusIcon(item.statusId) }}
+                </v-icon>
+              </v-chip>
+            </template>
+          </v-data-table>
+        </div>
+      </v-card-text>
+
+      <!-- Pagination -->
+      <v-card-text class="pa-2 pa-sm-3">
+        <div class="pagination-wrapper">
+          <span class="total-data-text">Total: {{ totalItems }}</span>
+          <v-pagination 
+            v-model="pages" 
+            :length="totalPage" 
+            @input="onPageChangeDetil" 
+            :total-visible="paginationVisible"
+            class="pagination-component"
+          ></v-pagination>
+        </div>
+      </v-card-text>
     </v-card>
 
-    <HelpdeskFormModal :open="helpdeskModal" :datas="datas" @close="closeHelpdeskModal"></HelpdeskFormModal>
+    <!-- Modal -->
+    <HelpdeskFormModal 
+      :open="helpdeskModal" 
+      :datas="datas" 
+      @close="closeHelpdeskModal"
+    ></HelpdeskFormModal>
   </div>
 </template>
 
@@ -156,6 +180,8 @@ import {
   mdiMagnify,
   mdiCheck,
   mdiCheckAll,
+  mdiDownload,
+  mdiPlus,
 } from "@mdi/js";
 
 export default {
@@ -175,6 +201,8 @@ export default {
         mdiMagnify,
         mdiCheck,
         mdiCheckAll,
+        mdiDownload,
+        mdiPlus,
       },
       pages: 1,
       totalPage: 1,
@@ -211,6 +239,44 @@ export default {
       status: null,
       isITLead: false,
     };
+  },
+  computed: {
+    statusCards() {
+      return [
+        { title: "All Tickets", value: this.allTickets, class: "all-ticket", icon: "mdiTicketConfirmationOutline", filter: null },
+        { title: "Assigned", value: this.assignedTickets, class: "assigned-ticket", icon: "mdiClipboardArrowRightOutline", filter: "ASSIGNED" },
+        { title: "Progress", value: this.progressTickets, class: "progress-ticket", icon: "mdiLoading", filter: "INPROGRESS" },
+        { title: "Pending", value: this.pendingTickets, class: "pending-ticket", icon: "mdiClipboardTextClockOutline", filter: "PENDING" },
+        { title: "Late", value: this.lateTickets, class: "late-ticket", icon: "mdiClipboardAlertOutline", filter: "LATE" },
+        { title: "Solved", value: this.solvedTickets, class: "solved-ticket", icon: "mdiCheckDecagramOutline", filter: "RESOLVED" },
+      ];
+    },
+    tableHeaders() {
+      if (this.$vuetify.breakpoint.xs) {
+        return [
+          { text: "Ticket", value: "number" },
+          { text: "Title", value: "title" },
+          { text: "Status", value: "statusName" },
+        ];
+      }
+      if (this.$vuetify.breakpoint.sm) {
+        return [
+          { text: "Ticket", value: "number" },
+          { text: "Date", value: "createdAt" },
+          { text: "Title", value: "title" },
+          { text: "Status", value: "statusName" },
+        ];
+      }
+      return this.headers;
+    },
+    iconSize() {
+      return this.$vuetify.breakpoint.xs ? 24 : 32;
+    },
+    paginationVisible() {
+      if (this.$vuetify.breakpoint.xs) return 3;
+      if (this.$vuetify.breakpoint.sm) return 5;
+      return 7;
+    }
   },
   created() {
     this.getUserData();
@@ -407,77 +473,244 @@ export default {
 </script>
 
 <style scoped>
-.btn-blue {
-  background-color: rgb(1, 114, 185) !important;
-  color: white;
-  text-transform: none;
+.helpdesk-container {
   width: 100%;
+}
+
+/* Status Cards */
+.status-cards-section {
+  background-color: #f1f1f7;
+  border-radius: 8px;
+  padding: 8px 4px;
+}
+
+.status-cards-row {
+  margin: 0 !important;
+}
+
+.status-col {
+  padding: 4px;
 }
 
 .detail-info {
   text-align: center;
   cursor: pointer;
+  transition: all 0.3s ease;
+  height: 100%;
+  border-radius: 8px;
+}
+
+.detail-info:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+}
+
+.status-title {
+  color: white;
+  font-size: 11px;
+  font-weight: 500;
+  line-height: 1.2;
+}
+
+.status-value {
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
 }
 
 .all-ticket {
-  background-color: #9a9a9a;
-  color: white;
+  background: linear-gradient(135deg, #9a9a9a 0%, #7a7a7a 100%);
 }
 
 .assigned-ticket {
-  background-color: #a11497;
+  background: linear-gradient(135deg, #a11497 0%, #810d77 100%);
 }
 
 .progress-ticket {
-  background-color: #0172b9;
+  background: linear-gradient(135deg, #0172b9 0%, #015a93 100%);
 }
 
 .pending-ticket {
-  background-color: #ffd401;
+  background: linear-gradient(135deg, #ffd401 0%, #e6be00 100%);
 }
 
 .late-ticket {
-  background-color: #ec323f;
+  background: linear-gradient(135deg, #ec323f 0%, #c9262f 100%);
 }
 
 .solved-ticket {
-  background-color: #adc43b;
+  background: linear-gradient(135deg, #adc43b 0%, #8fa62e 100%);
 }
 
+/* Main Card */
+.main-card {
+  border-radius: 8px;
+}
+
+/* Button */
+.btn-blue {
+  background-color: rgb(1, 114, 185) !important;
+  color: white;
+  text-transform: none;
+}
+
+/* Date Picker */
 .datetime-picker {
   width: 100%;
 }
 
 ::v-deep .mx-input {
   height: 40px !important;
+  font-size: 14px;
 }
 
-.status-badge {
-  color: white;
-  border-radius: 5px;
+/* Table */
+.table-container {
+  overflow-x: auto;
+  width: 100%;
+}
+
+::v-deep .v-data-table > .v-data-table__wrapper > table {
+  min-width: 100%;
+}
+
+::v-deep .v-data-table tbody tr {
+  cursor: pointer;
+}
+
+::v-deep .v-data-table tbody tr:hover {
+  background-color: #f5f5f5 !important;
+}
+
+/* Status Chip */
+.status-chip {
+  color: white !important;
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 11px;
+  height: 24px !important;
+}
+
+.status-text {
+  font-size: 11px;
 }
 
 .status-submitted {
-  background-color: #0172b9;
+  background-color: #0172b9 !important;
 }
 
 .status-inprogress {
-  background-color: #0172b9;
+  background-color: #0172b9 !important;
 }
 
 .status-pending {
-  background-color: #ff7a00;
+  background-color: #ff7a00 !important;
 }
 
 .status-assigned {
-  background-color: #a11497;
+  background-color: #a11497 !important;
 }
 
 .status-late {
-  background-color: #ec323f;
+  background-color: #ec323f !important;
 }
 
 .status-resolved {
-  background-color: #adc43b;
+  background-color: #adc43b !important;
+}
+
+/* Pagination */
+.pagination-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 12px;
+}
+
+.total-data-text {
+  font-weight: 600;
+  font-size: 13px;
+}
+
+/* Tablet */
+@media (max-width: 960px) {
+  .status-title {
+    font-size: 10px;
+  }
+
+  .status-value {
+    font-size: 20px;
+  }
+
+  ::v-deep .v-data-table th,
+  ::v-deep .v-data-table td {
+    font-size: 13px;
+    padding: 0 8px !important;
+  }
+}
+
+/* Mobile */
+@media (max-width: 600px) {
+  .status-cards-section {
+    padding: 4px 2px;
+  }
+
+  .status-col {
+    padding: 2px;
+  }
+
+  .status-title {
+    font-size: 9px;
+    margin-top: 4px !important;
+  }
+
+  .status-value {
+    font-size: 18px;
+    margin-top: 2px !important;
+  }
+
+  .detail-info .v-card__text {
+    padding: 8px !important;
+  }
+
+  .pagination-wrapper {
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+  }
+
+  .total-data-text {
+    font-size: 12px;
+    width: 100%;
+    text-align: center;
+  }
+
+  .pagination-component {
+    width: 100%;
+  }
+
+  ::v-deep .v-pagination__navigation,
+  ::v-deep .v-pagination__item {
+    min-width: 28px !important;
+    height: 28px !important;
+    margin: 1px !important;
+    font-size: 12px;
+  }
+
+  ::v-deep .v-data-table th,
+  ::v-deep .v-data-table td {
+    font-size: 11px !important;
+    padding: 0 4px !important;
+  }
+
+  .status-chip {
+    font-size: 10px;
+    height: 20px !important;
+  }
+
+  .status-text {
+    font-size: 10px;
+  }
 }
 </style>
